5e20eed96f80340a29d8ea0ce9199a712002125d
Alan Silva <alansq16@gmail.com>
2021-05-05 00:26:13 -0400

    Adding service resolution section

--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ import os
 import logging
 from dns_helpers import *
 from env_vars import get_env_var_int, get_required_env_var
-from services import get_services_with_annotation
-from headless import get_headless_endpoint_ips
+from services import get_services_with_annotation, resolve_service
 from kh_status import kh_fail, kh_success
-import sys
 from pprint import pprint
 
 # TODO: Make log level configurable from env var
@@ -23,26 +21,14 @@ logger.info("DNS_NAMESPACE env var provided as: '%s'", DNS_NAMESPACE)
 # Get the max number of services we will check for pagination
 MAX_SERVICES = get_env_var_int('MAX_SERVICES', 30)
 
-logger.info("Looking for services with annotation '%s'", ANNOTATION)
-_continue = None
-while True:
-    annotated_services = get_services_with_annotation(
-            annotation='please-check',
-            _continue=_continue,
-            limit=MAX_SERVICES
-            )
+# Verify that the Kubernetes master Service works. If it doesn't, then there's
+# no need to check anything else, we have bigger problems.
+# https://kubernetes.io/docs/tasks/debug-application-cluster/debug-service/#does-any-service-exist-in-dns
+logger.info("Attempting to resolve the kubernetes master service")
+ips_record = get_ips_record("kubernetes.default")
+logger.info("Service: 'kubernetes.default' resolved successfully")
 
-    if annotated_services['services']:
-        pprint(annotated_services['services'])
-
-    _continue = annotated_services['continue']
-
-    if not annotated_services['continue']:
-        break
-
-    logger.debug("%s services remaining to check", annotated_services['remaining'])
-
-##### DNS section
+### DNS section ###
 
 dns_svc = get_dns_svc(DNS_NAMESPACE, DNS_NODE_SELECTOR)
 
@@ -64,78 +50,31 @@ if set(dns_eps_ips).difference(dns_pod_ips):
 else:
     logger.info("Successfully matched DNS endpoint and pod IPs.")
 
-sys.exit(0)
+### SVC LOOKUP SECTION ###
 
-# Verify that the Kubernetes master Service works. If it doesn't, then there's
-# no need to check anything else, we have bigger problems.
-# https://kubernetes.io/docs/tasks/debug-application-cluster/debug-service/#does-any-service-exist-in-dns
+logger.info("Looking for services with annotation '%s'", ANNOTATION)
+_continue = None
 
-logger.info("Attempting to resolve the kubernetes master service")
+while True:
+    annotated_services = get_services_with_annotation(
+            annotation='please-check',
+            _continue=_continue,
+            limit=MAX_SERVICES
+            )
 
-# If we got the information to find the DNS service's IP use it explicitely as the nameserver
-# Otherwise, let the default nameserver be used
-if dns_svc_ip:
-    ips_record = get_ips_record("kubernetes.default", [dns_svc_ip])
-else:
-    ips_record = get_ips_record("kubernetes.default")
+    if annotated_services['services']:
+        pprint(annotated_services['services'])
+        for service in annotated_services['services']:
+            resolve_service(
+                service=service['name'],
+                namespace=service['namespace'],
+                cluster_ip=service['cluster_ip'],
+                nameservers=dns_pod_ips
+            )
 
-logger.info("Service: 'kubernetes.default' resolved successfully")
+    _continue = annotated_services['continue']
 
-# Check if the HOSTNAME is qualified, if so, extract the hostname and namespace.
+    if not annotated_services['continue']:
+        break
 
-hostname_list = HOSTNAME.split('.')
-
-if len(hostname_list) > 1:
-    hostname = hostname_list[0]
-    hostname_namespace = hostname_list[1]
-    logger.info("HOSTNAME: '%s' seems to be qualified. Inferring namespace: '%s'", HOSTNAME, hostname_namespace)
-else:
-    hostname = HOSTNAME
-    hostname_namespace = 'default'
-    logger.info("Non-qualified HOSTNAME: '%s' was given. Assuming namespace: '%s'", HOSTNAME, hostname_namespace)
-
-logger.info("Looking for HOSTNAME: '%s' in namespace: '%s'", hostname, hostname_namespace)
-
-hostname_svc = get_namespaced_service(hostname_namespace, field=f"metadata.name={hostname}")
-
-if not hostname_svc.items:
-    kh_fail(f"Could not find service '{hostname}'. Check that it exists in the '{hostname_namespace}' namespace")
-
-hostname_svc_ip = hostname_svc.items[0].spec.cluster_ip
-logger.info("Found service '%s' with ClusterIP: '%s'", HOSTNAME, hostname_svc_ip)
-
-#TODO: I feel like this could be refactored to be made simpler... need to come back to this later
-
-# If we were able to obtain the nameserver IPs, verify they are working correctly
-if dns_eps_ips:
-    # If we're dealing with a headless svc, get the endpoint IP list and use it to compare against the resolved IPs
-    # Otherwise, just use the normal non-headless clusterIP that we obtained previously
-    if hostname_svc_ip == 'None':
-        comparison_ips = get_headless_endpoint_ips(hostname_namespace, hostname)
-        logger.info("Found endpoint IPs %s for headless service '%s'", comparison_ips, HOSTNAME)
-    else:
-        comparison_ips = [hostname_svc_ip]
-
-    logger.info("Attempting to resolve '%s' with all DNS nameservers previously found", HOSTNAME)
-    for nameserver in dns_eps_ips:
-        ips_record = get_ips_record(HOSTNAME, [nameserver])
-        ips_record_answer = sorted(ips_record.answer)
-        logger.info("Nameserver '%s' resolved hostname to %s", nameserver, ips_record_answer)
-        if set(ips_record_answer).difference(comparison_ips):
-            kh_fail(f"Nameserver: '{nameserver}' resolved IP '{ips_record_answer}' when the expected IP was '{comparison_ips}'")
-    logger.info("All nameservers resolved '%s' correctly!", HOSTNAME)
-else:
-    # If we didn't have the info to find the DNS servers and we were provided a non-headless service, at least try to resolve it with the default nameserver
-    # There's nothing we can do about non-headless services...
-    if hostname_svc_ip != 'None':
-        ips_record = get_ips_record(HOSTNAME)
-
-        ips_record_answer = sorted(ips_record.answer)
-        comparison_ip = [hostname_svc_ip]
-
-        if set(ips_record_answer).difference(comparison_ip):
-            kh_fail(f"Default nameserver resolved IP '{ips_record_answer}' when the expected IP was '{comparison_ip}'")
-        else:
-            logger.info("Default nameserver resolved '%s' to %s which is correct!", HOSTNAME, ips_record_answer)
-
-kh_success()
+    logger.debug("%s services remaining to check", annotated_services['remaining'])
--- app/services.py
+++ app/services.py
@@ -1,10 +1,23 @@
 import logging
 from kh_status import kh_fail
-from kube_helpers import get_all_services
+from kube_helpers import get_all_services, get_namespaced_endpoints
+from dns_helpers import get_ips_record
 
 # TODO: Make log level configurable from env var
 logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def get_headless_endpoint_ips(headless_svc, namespace):
+    logger.info("Getting the endpoint IPs for headless service: '%s'", headless_svc)
+
+    headless_svc_eps = get_namespaced_endpoints(namespace, f"metadata.name={headless_svc}")
+
+    headless_eps_ips = []
+    for subset in headless_svc_eps.items[0].subsets:
+        for address in subset.addresses:
+            headless_eps_ips.append(address.ip)
+
+    return sorted(headless_eps_ips)
 
 def get_services_with_annotation(annotation=None, _continue=None, limit=None):
     services = get_all_services(_continue=_continue, limit=limit)
@@ -24,3 +37,21 @@ def get_services_with_annotation(annotation=None, _continue=None, limit=None):
             })
 
     return filtered
+
+def resolve_service(service, namespace, cluster_ip, nameservers):
+
+    qualified_service = f"{service}.{namespace}"
+
+    if cluster_ip:
+        comparison_ips = [cluster_ip]
+    else:
+        comparison_ips = get_headless_endpoint_ips(service, namespace)
+
+    for nameserver in nameservers:
+        ips_record = get_ips_record(qualified_service, [nameserver])
+        ips_record_answer = sorted(ips_record.answer)
+
+        if set(ips_record_answer).difference(comparison_ips):
+            kh_fail(f"Nameserver: '{nameserver}' resolved IP '{ips_record_answer}' when the expected IP was '{cluster_ip}'")
+
+        logger.info("All nameservers resolved '%s' correctly!", qualified_service)
